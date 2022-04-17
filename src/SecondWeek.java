852f0f058dd950e488af75a99e41f1a20465f3ef
ishwar46 <ishwarura@gmail.com>
2022-04-17 11:22:01 +0545

    Voting

--- src/SecondWeek.java
+++ src/SecondWeek.java
@@ -1,3 +1,5 @@
+//Working with Operators
+
 public class SecondWeek {
     public static void main(String[] args) {
         System.out.println("Week 2");
---
+++ src/VotingSystem.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class VotingSystem {
+    public static void main(String[] args) {
+        float progammming, database, android, math, communication;
+        double total, average, percentage;
+        Scanner std=new Scanner(System.in);
+
+        System.out.print("Enter marks of progammming :");
+        progammming=std.nextFloat();
+        System.out.print("Enter marks of database :");
+        database=std.nextFloat();
+        System.out.print("Enter marks of android :");
+        android=std.nextFloat();
+        System.out.print("Enter marks of maths :");
+        math=std.nextFloat();
+        System.out.print("Enter marks of communication :");
+        communication=std.nextFloat();
+
+        total = progammming + database + android + math + communication;
+        average = (total / 5.0);
+        percentage = (total / 500.0) * 100;
+
+
+        System.out.println("Total marks ="+total);
+        System.out.println("Average marks = "+average);
+        System.out.println("Percentage = "+percentage);
+        if (percentage >= 70) {
+            System.out.println("First Class");
+        } else if (percentage <= 59) {
+            System.out.println("Upper Second Class");
+        } else if(percentage <=49 ){
+            System.out.println("Second Class");
+        }
+        else {
+
+            System.out.println("Third Class");
+        }
+    }
+}
