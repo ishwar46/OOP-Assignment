3090324c4325f6d8d6ee62087dc7da45c1317c92
ishwar46 <ishwarura@gmail.com>
2022-04-18 22:46:52 +0545

    update

---
+++ src/CodeDope.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CodeDope {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter fname");
+        String a = sc.nextLine();
+        System.out.println("Enter lname");
+        String n = sc.nextLine();
+        System.out.print(a+n);
+    }
+
+
+}
