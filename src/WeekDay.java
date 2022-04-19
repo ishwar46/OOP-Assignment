346ae3be52dacd0bd248624fac4f664689842bfe
ishwar46 <ishwarura@gmail.com>
2022-04-19 20:30:39 +0545

    WeekDay

---
+++ src/WeekDay.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class WeekDay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter weekday number : ");
+        int day = sc.nextInt();
+
+        if(day == 1) {
+
+            System.out.println("Monday");
+
+        } else if(day == 2) {
+
+            System.out.println("Tuesday");
+
+        } else if(day == 3) {
+
+            System.out.println("Wednesday");
+
+        } else if(day == 4) {
+
+            System.out.println("Thursday");
+
+        } else if(day == 5) {
+
+            System.out.println("Friday");
+
+        } else if(day == 6) {
+
+            System.out.println("Saturday");
+
+        } else if(day == 7) {
+
+            System.out.println("Sunday");
+
+        } else {
+
+            System.out.println("Please enter weekday number between 1-7.");
+        }
+    }
+}
+
+
+
