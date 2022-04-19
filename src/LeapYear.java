59342c0130e9f5fc8ce17351ecd0e3cc0089eee1
ishwar46 <ishwarura@gmail.com>
2022-04-19 15:14:09 +0545

    LeapYearUpdate

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,7 +1,18 @@
 //Write a JAVA program to check whether a year is leap year or not.
 
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any year to check if it is Leap Year or not: ");
+        int year = sc.nextInt();
 
+        if(((year % 4 == 0) && (year % 100!=0)) || (year % 400 == 0)){
+            System.out.println("Entered year " +year+ " is a LeapYear");
+        }
+        else{
+            System.out.println("Entered number " +year+ " is not a leapyear");
+        }
     }
 }
