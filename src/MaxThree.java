a8252742fa8ac10dc207b27567104348e7501f47
ishwar46 <ishwarura@gmail.com>
2022-04-19 14:24:18 +0545

    MaxThree

---
+++ src/MaxThree.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaxThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num2: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter num3");
+        int num3 = sc.nextInt();
+
+        if(num1 > num2)
+        {
+            System.out.println(num1+ "is greater");
+        }
+        else if(num2 > num3)
+        {
+            System.out.println(num2+ "is greater");
+        }
+        else if(num3 > num1){
+            System.out.println(num3+ "is greater");
+        }
+    }
+}
