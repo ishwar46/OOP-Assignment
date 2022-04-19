d21a13f5d3fd40b264e3ae7402793d85fad4b37c
ishwar46 <ishwarura@gmail.com>
2022-04-19 18:08:02 +0545

    Alphabet

--- src/AlphabetCheck.java
+++ src/AlphabetCheck.java
@@ -3,5 +3,18 @@ import java.util.Scanner;
 public class AlphabetCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        char alphabet = sc.next().charAt(0);
+
+        if((alphabet>='A' && alphabet<='Z') || (alphabet >= 'a' && alphabet>='z'))
+        {
+            System.out.println(alphabet+ " is an Alphabet");
+        }
+        else{
+            System.out.println(alphabet+ " is not an Alphabet");
+
+        }
+
+
     }
 }
