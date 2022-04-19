4a658437bbdc15cb23cabb7decf5cb585d07ec95
ishwar46 <ishwarura@gmail.com>
2022-04-19 20:00:47 +0545

    Create Vowels.java

---
+++ src/Vowels.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Vowels {
+    public static void main(String args[]){
+        char ch;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an Alphabet: ");
+        ch=sc.next().charAt(0);
+        if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z')){ //outer if use to check consonant
+
+            if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+            {//inner if statements checks vowels
+                System.out.print(ch+" is vowel");
+            }
+            else{//display consonant
+                System.out.print(ch+" is consonant");
+            }
+        }
+        else{
+            System.out.print(ch+" is neither a vowel nor a consonant");
+        }
+    }
+}
