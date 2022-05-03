f241a06861525474224f4fdc44970da60b432efc
ishwar46 <ishwarura@gmail.com>
2022-05-03 21:08:17 +0545

    Practice

---
+++ src/Practice.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//Marks of student
+public class Practice {
+    public static void main(String[] args) {
+        float total, percentage;
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        System.out.println("");
+
+        System.out.println("Enter the marks for maths: ");
+        int math = sc.nextInt();
+
+        System.out.println("Enter the marks for physics: ");
+        int phy = sc.nextInt();
+
+        System.out.println("Enter the marks for programming: ");
+        int programming = sc.nextInt();
+
+        System.out.println("Enter the marks for database: ");
+        int database = sc.nextInt();
+
+        System.out.println("Enter the marks for database: ");
+        int android = sc.nextInt();
+
+        total = (math+phy+programming+database+android);
+        percentage = total/5;
+
+        System.out.println("The obtained marks is: " +total);
+        System.out.println("The obtained percentage is: " +percentage);
+
+
+    }
+}
+
