c391637454f6578ac92d27915d6d0964527efab7
ishwar46 <ishwarura@gmail.com>
2022-05-10 05:53:51 +0545

    Logical Operators

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -4,3 +4,32 @@ public class RandomPractice {
         System.out.println("Practice Makes a Man Perfect");
     }
 }
+
+//Conditional Statements
+
+class Age{
+    public static void main(String[] args) {
+        int age = 20;
+        if(age>18){
+            System.out.println("You can drive ");
+        }
+        else {
+            System.out.println("You cannot drive!!");
+        }
+    }
+}
+
+//Logical Operators
+
+class LogicalOps{
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        if (a && b){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
