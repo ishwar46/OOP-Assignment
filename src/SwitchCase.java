97d3d7a84bf166266426c5935ed9d6931175442e
ishwar46 <ishwarura@gmail.com>
2022-05-10 06:28:06 +0545

    SwitchCase

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 //Switch Case
 public class SwitchCase {
+    public static void main(String[] args) {
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        age = sc.nextInt();
+
+        switch (age){
+            case 15:
+                System.out.println("You cannot have your Driving Licence.");
+                break;
+            case 18:
+                System.out.println("You can have your Driving Licence.");
+                break;
+            case 90:
+                System.out.println("We recommend you not to drive");
+                break;
+            default:
+                System.out.println("Invalid!");
+        }
+    }
 }
