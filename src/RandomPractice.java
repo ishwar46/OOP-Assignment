8caf33aa68e75537b5558dc978f12ad9a3d84330
ishwar46 <ishwarura@gmail.com>
2022-05-10 06:01:35 +0545

    Logical Ops

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -23,13 +23,42 @@ class Age{
 
 class LogicalOps{
     public static void main(String[] args) {
+        System.out.println("For Logical AND");
         boolean a = true;
-        boolean b = true;
-        if (a && b){
-            System.out.println("Yes");
+        boolean b = false;
+        boolean c = true;
+        if (a && b && c){
+            System.out.println("True");
         }
         else {
-            System.out.println("No");
+            System.out.println("False");
         }
+    }
+}
+
+class LogicalOR{
+    public static void main(String[] args) {
+        System.out.println("Logical OR");
+        boolean a = true;
+        boolean b = false;
+        //boolean c = true;
+        if(a || b){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
+
+class LogicalNOT{
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("For Logical NOT");
+        System.out.println("Not(A) is ");
+        System.out.println(!a);
+        System.out.println("Not(B) is ");
+        System.out.println(!b);
     }
 }
