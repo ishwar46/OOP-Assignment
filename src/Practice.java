38dba3831f13369d257c85c0bad3f069b2c31aa2
ishwar46 <ishwarura@gmail.com>
2022-05-10 06:32:17 +0545

    CheckNumber

--- src/Practice.java
+++ src/Practice.java
@@ -34,3 +34,14 @@ public class Practice {
     }
 }
 
+//
+class CheckNumber{
+    public static void main(String[] args) {
+        int a = 10;
+        if(a==11)
+            System.out.println("Iam 11");
+        else {
+            System.out.println("Iam not 11");
+        }
+    }
+}
