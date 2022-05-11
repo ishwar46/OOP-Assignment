8de307bb06da135c244642d29e2a6ec814826363
ishwar46 <ishwarura@gmail.com>
2022-05-11 16:16:40 +0545

    While Loops

---
+++ src/Practice/WhileLoops.java
@@ -0,0 +1,25 @@
+package Practice;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        System.out.println("While Loop");
+        int i = 1;
+        while (i<=3) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Loop Ends");
+        while (true){
+            System.out.println("Having Fun!");
+        }
+    }
+}
+class Hundred{
+    public static void main(String[] args) {
+        int i = 100;
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
