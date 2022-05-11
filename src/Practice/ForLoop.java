f5fea7eef03a2c423e527970f7052f3ff9ec1aa4
ishwar46 <ishwarura@gmail.com>
2022-05-11 18:50:26 +0545

    For Loop

---
+++ src/Practice/ForLoop.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+//First n odd numbers using for loop
+
+/*
+class OddNumber{
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 0; i<n; i++);
+        {
+            System.out.println(2*i+1);
+        }
+    }
+}*/
