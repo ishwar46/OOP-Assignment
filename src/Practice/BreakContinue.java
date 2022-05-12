5cd5f6e9b32befa2bbc8c365ab02ee165d417247
ishwar46 <ishwarura@gmail.com>
2022-05-12 19:25:35 +0545

    Update Break

--- src/Practice/BreakContinue.java
+++ src/Practice/BreakContinue.java
@@ -1,4 +1,16 @@
+//Break and Continue using loops
 package Practice;
 
 public class BreakContinue {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<5;i++){
+            System.out.println(i);
+            System.out.println("Hello World");
+            if(i==2){
+                System.out.println("Matched!");
+                break;
+            }
+        }
+    }
 }
