3c8b6038ae360f6630f5319d1edd22afa7deda4c
ishwar46 <ishwarura@gmail.com>
2022-05-19 13:30:15 +0545

    Update MatrixAdd.java

--- src/Week6/MatrixAdd.java
+++ src/Week6/MatrixAdd.java
@@ -79,47 +79,44 @@ public class MatrixAdd {
 }
 
 
-class TestArray{
+class AddArray{
     public static void main(String args[]){
-//creating two matrices
+
         int a[][]={{1,3,4},{3,4,5}};
         int b[][]={{1,3,4},{3,4,5}};
 
-//creating another matrix to store the sum of two matrices
         int c[][]=new int[2][3];
 
-//adding and printing addition of 2 matrices
         for(int i=0;i<2;i++){
             for(int j=0;j<3;j++){
                 c[i][j]=a[i][j]+b[i][j];
                 System.out.print(c[i][j]+" ");
             }
-            System.out.println();//new line
+            System.out.println();
         }
-
     }
 }
 
 class MatrixMultiplication{
     public static void main(String args[]){
-//creating two matrices
+
         int a[][]={{1,1,1},{2,2,2},{3,3,3}};
         int b[][]={{1,1,1},{2,2,2},{3,3,3}};
 
-//creating another matrix to store the multiplication of two matrices
-        int c[][]=new int[3][3]; //3 rows and 3 columns
 
-//multiplying and printing multiplication of 2 matrices
+        int c[][]=new int[3][3];
+
+
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 c[i][j]=0;
                 for(int k=0;k<3;k++)
                 {
                     c[i][j]+=a[i][k]*b[k][j];
-                }//end of k loop
-                System.out.print(c[i][j]+" "); //printing matrix element
-            }//end of j loop
-            System.out.println();//new line
+                }
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
